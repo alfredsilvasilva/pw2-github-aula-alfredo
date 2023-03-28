3d1a1141bde58677bb8402a52cb799ea06c049fe
alfredsilvasilva <alfredmetal@gmail.com>
2023-03-27 22:14:30 -0300

    outro commit

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
  const app = express();
 
  app.get('/',(req, res)=>{
-    res.send('Ola')
+    res.send('<h1>Ola</h1>')
  })
-
+S
  app.listen(3000, ()=>{
     console.log('Ola mundo' +' servidor  rodando na porta 3000')
  });
